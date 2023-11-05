340bdcca579fcd6117681adc4d041e5c40dba3c5
junnn0021 <junnn0021@gmail.com>
2023-11-06 07:10:46 +0900

    cartservice 소스 코드 초기화

--- src/cartservice/src/Startup.cs
+++ src/cartservice/src/Startup.cs
@@ -22,13 +22,14 @@ namespace cartservice
 
         public IConfiguration Configuration { get; }
         
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             string redisAddress = Configuration["REDIS_ADDR"];
             string spannerProjectId = Configuration["SPANNER_PROJECT"];
             string spannerConnectionString = Configuration["SPANNER_CONNECTION_STRING"];
             string alloyDBConnectionString = Configuration["ALLOYDB_PRIMARY_IP"];
-            string mysqlConnectionString = Configuration["MYSQL_CONNECTION_STRING"];
 
             if (!string.IsNullOrEmpty(redisAddress))
             {
@@ -47,11 +48,6 @@ namespace cartservice
                 Console.WriteLine("Creating AlloyDB cart store");
                 services.AddSingleton<ICartStore, AlloyDBCartStore>();
             }
-            else if (!string.IsNullOrEmpty(mysqlConnectionString))
-            {
-                Console.WriteLine("Creating MySQL cart store");
-                services.AddSingleton<ICartStore, MySqlCartStore>();
-            }
             else
             {
                 Console.WriteLine("Redis cache host(hostname+port) was not specified. Starting a cart service using in memory store");
@@ -59,9 +55,11 @@ namespace cartservice
                 services.AddSingleton<ICartStore, RedisCartStore>();
             }
 
+
             services.AddGrpc();
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
